c48f26aa7b8987e909a6115f3600b332e57075bc
Evan Brass <ebrass@groupon.com>
2020-06-02 10:03:55 -0700

    Initial Commit

---
+++ Forecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Forecast
+//
+//  Created by Evan Brass on 6/2/20.
+//  Copyright © 2020 Evan Brass. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
